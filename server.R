6be3b3b7d7220f3878f3fb91c5dd0d2974b53613
Elo Lewis <elo.lewis@ntrepidcorp.com>
2019-03-22 15:47:34 -0400

    update source files

--- server.R
+++ server.R
@@ -1,43 +1,39 @@
+source("helpers.R", local = TRUE)
+source("postgres.R", local = TRUE)
 
 server <- function(input, output, session) {
     observeEvent(input$retrieve_user, {
-      testvector <- transpose(as.data.table(c(input$username, input$password)))
-      if(NROW(merge(testvector,
-                    matrixa, 
-                    by.x = c("V1", "V2"), 
-                    by.y = c("email", "password")))==1){
-           # characters tab -----
-            insertTab(inputId = "tabs",
-                      target = "How to Play",
-                      tabPanel("Characters", 
-                               fluidPage(
-                                 includeMarkdown('characters_panel.md')
-                                 )
-                               )
-                      )
-           # campaign tab -----
-            insertTab(inputId = "tabs",
-                      target = "Characters",
-                      tabPanel("Campaigns", 
-                               fluidPage(
-                                 includeMarkdown('campaigns_panel.md')
-                                 )
-                               )
-                      )
-           # characters tab -----
-            insertTab(inputId = "tabs",
-                      target = "Campaigns",
-                      tabPanel("Rules", 
-                               fluidPage(
-                                 includeMarkdown('full_rules.md')
-                                 )
-                               )
-            )
-           } else {textOutput("Username and/or password not found")}
-        }) # Render additional tabs on login
-    observeEvent(input$create_user {
-      # check if username is unique
-      if (input$username %in% )
-
-    }) # Add unique username and pass to database
+    
+    if(loginTest(input$username, input$passkey)) {
+      output$messages <- renderText({"Logged in"})
+      appendTab(inputId = "tabs", # characters tab -----
+                # target = "howtoplay",
+                tabPanel("Characters", 
+                         fluidPage(
+                           includeMarkdown('characters_panel.md')
+                           )
+                         )
+                )       # characters tab
+      appendTab(inputId = "tabs", # campaign tab -----
+                # target = "Characters",
+                tabPanel("Campaigns", 
+                         fluidPage(
+                           includeMarkdown('campaigns_panel.md')
+                           )
+                         )
+                ) # campaign tab
+      appendTab(inputId = "tabs", # Rules tab -----
+                # target = "Campaigns",
+                tabPanel("Rules", 
+                         fluidPage(
+                           includeMarkdown('full_rules.md')
+                           )
+                         )
+                ) # Rules tab
+    } else {
+      output$messages <- renderText({"Username and/or password not found"})
+        }
+    }) # Render additional tabs on login
+  
   }
+
