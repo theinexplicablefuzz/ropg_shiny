23264e8c93863e703d437e2f9624d3a9da19a641
Elo Lewis <elo.lewis@ntrepidcorp.com>
2019-03-22 09:17:07 -0400

    update config output and remove strings as factors

--- config.R
+++ config.R
@@ -2,30 +2,39 @@
 
 library(data.table)
 
-# function to import config files as data table
+# function to import config files as data table.
 read_config <- function (file) {
   dframe <- read.table(file=file,header=FALSE,
-                       sep=' ')
+                       sep=' ',
+                       stringsAsFactors = FALSE)
   return(as.data.table(dframe))
 }
 
+# the configure function loads secret details from config.txt.
+# If there is no config.txt file, it creates one using fields from config default
+# and directs the user to fill in missing fields
+# depends on 'read_config' function above
 configure <- function() {
-  # load default config file to check required valuse
+  
+  # load default config
   config <- read_config("config_default.txt")
   
-  # if config.R exists then merge with config_default.R to update new fields
+  # check if config.R exists then merge with config_default.R to update new fields
   if(file.exists("config.txt")) {
     config_custom <- read_config("config.txt")
     config <- merge(config,config_custom, by = 'V1', all.x = TRUE)
     if(!is.null(config$V2.y)) {config <- config[,c(1,3)]}
   }
-  # save updated output
+  # save updated output to 'config.txt'
   write.table(config,"config.txt", row.names = FALSE, col.names = FALSE)
   names(config) <- c("key", "value")
   
-  # prompt the user for missing values
+  # prompt the user for missing fields
   for (i in config[is.na(value)]$key){
     return(stop(paste0("'",i,"'", " not found in config.txt. Please update your local config file")))
   }
-  return(config)
+  output_vector <- config$value
+  names(output_vector) <- config$key
+  
+  return(output_vector)
 }
